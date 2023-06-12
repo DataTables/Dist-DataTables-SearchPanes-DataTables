e5ea9292ce228e4418a47e8c4135d85ffbcc291b
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-06-12 15:51:32 +0100

    ee694b9c7571a8232e3e63d8a4dc31c6a6ca2496 New: Dark mode support for DataTables and Bootstrap 5.3+ styling
    6551d9edc9b845960b11ec383d8156de13c3ee58 Example: Update base example to have vanilla JS init
    
    Not updating all yet as they will need to be updated for `layout` anyway
    
    Sync to source repo @6551d9edc9b845960b11ec383d8156de13c3ee58

--- js/searchPanes.dataTables.min.mjs
+++ js/searchPanes.dataTables.min.mjs
@@ -1,4 +1,4 @@
 /*! Bootstrap integration for DataTables' SearchPanes
  * © SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-dt";import SearchPanes from"datatables.net-searchpanes";export default DataTable;
+import jQuery from"jquery";import DataTable from"datatables.net-dt";import SearchPanes from"datatables.net-searchpanes";let $=jQuery;export default DataTable;
